1f3591eb5d47a9552f749c92b05e23998060db8f
Kia Farhang <kfarhang0@gmail.com>
2018-03-15 15:00:28 -0500

    Clean up build processes for landing page, article TS/JS

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         extensions: ['.ts', '.js']
     },
     output: {
-        filename: 'bundle.js',
+        filename: 'spurs-nation.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
